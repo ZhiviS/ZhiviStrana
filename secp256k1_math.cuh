6cffabde985901f83ba7e61ed11330b1ef824d5d
ZhiviS <121164778+ZhiviS@users.noreply.github.com>
2025-12-22 17:57:26 +0300

    Update secp256k1_math.cuh

--- secp256k1_math.cuh
+++ secp256k1_math.cuh
@@ -1,18 +1,18 @@
 #ifndef SECP256K1_MATH_CUH
 #define SECP256K1_MATH_CUH
-
+#include "secp256k1_math.cuh"
 typedef uint32_t Big256[8];
 
 typedef struct {
     Big256 X, Y, Z;
 } PointJ;
 
-// Объявления функций
+// Объявления с extern "C" для совместимости
 __device__ void scalar_mul(const Big256 scalar, PointJ* res);
 
-__global__ void init_base_pub_kernel(const Big256 d_min, PointJ* out_base_pub);
+extern "C" __global__ void init_base_pub_kernel(const Big256 d_min, PointJ* out_base_pub);
 
-__global__ void search_kernel_incremental(
+extern "C" __global__ void search_kernel_incremental(
     PointJ base_pub_jac_in,
     const Big256 step,
     uint64_t start_offset,
