5465d6537b619c84233ef722209105f593f2b235
ZhiviS <121164778+ZhiviS@users.noreply.github.com>
2025-12-22 17:40:03 +0300

    Update secp256k1_math.cuh

--- secp256k1_math.cuh
+++ secp256k1_math.cuh
@@ -1,12 +1,26 @@
-#pragma once
-#include <cstdint>
+#ifndef SECP256K1_MATH_CUH
+#define SECP256K1_MATH_CUH
 
 typedef uint32_t Big256[8];
 
-extern "C" __global__ void search_pubkeys(
-    const Big256* priv_keys,
-    uint8_t target_prefix,
-    const uint8_t* target_x_prefix,
+typedef struct {
+    Big256 X, Y, Z;
+} PointJ;
+
+// Объявления функций
+__device__ void scalar_mul(const Big256 scalar, PointJ* res);
+
+__global__ void init_base_pub_kernel(const Big256 d_min, PointJ* out_base_pub);
+
+__global__ void search_kernel_incremental(
+    PointJ base_pub_jac_in,
+    const Big256 step,
+    uint64_t start_offset,
+    uint64_t iterations_per_thread,
+    const uint8_t* d_target_x_prefix,
     int prefix_len,
-    unsigned long long* matches
+    uint8_t target_prefix,
+    unsigned long long* d_matches
 );
+
+#endif
